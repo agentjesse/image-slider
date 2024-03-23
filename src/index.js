ecb629ecbdb1019f8953460801a6d7a50592475a
jesse <31576321+agentjesse@users.noreply.github.com>
2024-03-23 17:25:23 -0400

    slider auto advances now

--- src/index.js
+++ src/index.js
@@ -9,24 +9,47 @@ import './styles.css';
 // module imports, from named and default
 import { logToConsole as lg, tableToConsole as tb } from './logger'; //shorthand loggers
 
-const viewRollImage = ( imageIndex=> {
-  //// Get the root element (usually <html>)
-  const root = document.documentElement;
-  // set bubbling evt listener
+const imageSliderSetup = ( ()=> {
+  let imageIndex = 0; //control index for zero-indexed images
+  const totalImages = 5; //match to .image divs
+
+  //change CSS variable after logic used to change imageIndex
+  const setCSSVarImageIndex = ()=> document.documentElement.style.setProperty( '--imageIndex', imageIndex );
+
+  // listener for next/previous image buttons
   document.querySelector('.sliderFrame').addEventListener( 'click', e=> {
+    e.stopPropagation();
     switch (e.target.className) {
-      case 'goLeft':
-        lg(e.target.className);
+      case 'previous':
+        if (imageIndex === 0) { //at first image
+          imageIndex = totalImages - 1; //go to last by setting new control index
+          setCSSVarImageIndex();
+        } else {
+          --imageIndex;
+          setCSSVarImageIndex();
+        }
         break;
-      case 'goRight':
-        lg(e.target.className);
+      case 'next':
+        if (imageIndex === totalImages - 1) { //at last image
+          imageIndex = 0; //go to first
+          setCSSVarImageIndex();
+        } else {
+          ++imageIndex;
+          setCSSVarImageIndex();
+        }
     }
   } );
 
-  // Set a CSS variable
-  // root.style.setProperty('--my-color', 'blue');
+  //auto advance to next image every 5 seconds
+  setInterval( ()=> {
+    if (imageIndex === totalImages - 1) { //at last image
+      imageIndex = 0; //go to first
+      setCSSVarImageIndex();
+    } else {
+      ++imageIndex;
+      setCSSVarImageIndex();
+    }
+  }, 5000 ); //every 5 seconds
 
-  return {
-    test: 'hi'
-  };
+  // return { test: 'hi' };
 } )();
